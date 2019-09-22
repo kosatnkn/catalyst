27a1061e7a4820490f3229de6e105630f5fcf30e
Kosala Tennakoon <kosala.me@gmail.com>
2019-09-22 15:20:24 +0530

    added config parser tests

---
+++ app/config/reader_test.go
@@ -0,0 +1,18 @@
+package config
+
+// func TestRead(t *testing.T) {
+
+// 	// input
+// 	file := "../../config/app.yaml"
+
+// 	// run
+// 	c := read(file)
+
+// 	// check
+// 	need := reflect.TypeOf([]byte)
+// 	got := reflect.TypeOf(c)
+
+// 	if got != need {
+// 		t.Errorf("Required %v, got %v", need, got)
+// 	}
+// }
