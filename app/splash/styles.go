b5230e9cda40dba144f92e9e428b2ebc1778df52
Kosala Tennakoon <kosala.me@gmail.com>
2019-10-12 12:06:25 +0530

    updated styles

--- app/splash/styles.go
+++ app/splash/styles.go
@@ -1,7 +1,7 @@
+package splash
+
 // More is available at following link.
 // https://www.kammerl.de/ascii/AsciiSignature.php
-
-package splash
 
 // StyleShadow shadow style
 const StyleShadow string = `
