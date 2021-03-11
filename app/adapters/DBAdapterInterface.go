44336c9d125f29d303531e1cd2eef51ffdf05c1e
Kosala Tennakoon <kosala.me@gmail.com>
2021-03-11 08:27:21 +0530

    added a todo

--- app/adapters/DBAdapterInterface.go
+++ app/adapters/DBAdapterInterface.go
@@ -3,6 +3,10 @@ package adapters
 import "github.com/kosatnkn/db"
 
 // DBAdapterInterface is implemented by all database adapters.
+//
+// TODO: Need to do a concrete definition here. Otherwise when the contract of this embedded interface changes the
+// app will break. So we need to do the definition we want here. Whether that matches exactly with the interface
+// presented by the third party package is an entirely different matter.
 type DBAdapterInterface interface {
 	db.AdapterInterface
 }
