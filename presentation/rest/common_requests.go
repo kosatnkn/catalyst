bde312b06839af3bf991f8ef8aabd635da48eade
Kosala Tennakoon <kosala.me@gmail.com>
2025-10-28 09:37:06 +0530

    fea: moved filter and pagination extraction to common_requests.go

--- presentation/rest/common_requests.go
+++ presentation/rest/common_requests.go
@@ -1,5 +1,75 @@
 package rest
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// filters extracts filter data from the provided param and send it as a map.
+func filters(param string) (map[string]any, error) {
+	f := map[string]any{}
+	if param == "" {
+		return f, nil
+	}
+
+	var arr []string
+	if err := json.Unmarshal([]byte(param), &arr); err != nil {
+		return f, errors.Join(errors.New("rest: error extracting filters from query param"), err)
+	}
+
+	for _, item := range arr {
+		parts := strings.SplitN(item, ":", 2) // only split into 2 parts
+		if len(parts) != 2 {
+			return f, fmt.Errorf("rest: filter '%s' is of invalid format", item)
+		}
+
+		key := strings.TrimSpace(parts[0])
+		val := strings.TrimSpace(parts[1])
+
+		f[key] = val
+	}
+
+	return f, nil
+}
+
+// paging extracts pagination data from the provided param and send it as a map.
+func paging(param string) (map[string]uint32, error) {
+	p := map[string]uint32{
+		"page": 1,
+		"size": 10,
+	}
+	if param == "" {
+		return p, nil
+	}
+
+	var arr []string
+	if err := json.Unmarshal([]byte(param), &arr); err != nil {
+		return p, errors.Join(errors.New("rest: error extracting 'paging' from query param"), err)
+	}
+
+	for _, item := range arr {
+		parts := strings.SplitN(item, ":", 2) // only split into 2 parts
+		if len(parts) != 2 {
+			return p, fmt.Errorf("rest: '%s' is of invalid format", item)
+		}
+
+		key := strings.TrimSpace(parts[0])
+		val := strings.TrimSpace(parts[1])
+		v, err := strconv.ParseUint(val, 10, 32)
+		if err != nil {
+			return p, fmt.Errorf("rest: paging '%s' is of invalid format", item)
+		}
+
+		p[key] = uint32(v)
+	}
+
+	return p, nil
+}
+
+// accountRequest maps account details sent in the request payload.
 type accountRequest struct {
 	Owner    string  `json:"owner" binding:"required"`
 	Currency string  `json:"currency"`
