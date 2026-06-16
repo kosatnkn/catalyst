a17bdcfe07af48e778ecf662301c151da269a54e
Kosala Tennakoon <kosala.me@gmail.com>
2026-06-16 22:49:22 +0530

    task: renamed loggingCtx to lctx

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 
 func main() {
 	// create a single context to be used by all logging calls
-	loggingCtx := context.Background()
+	lctx := context.Background()
 
 	// parse all configurations
 	settings := config.Settings{
@@ -46,7 +46,7 @@ func main() {
 		msgNPanic(nil, errors.Join(errors.New("main: error resolving container"), err).Error())
 	}
 	defer func() {
-		ctr.Logger.Info(loggingCtx, "destroying container")
+		ctr.Logger.Info(lctx, "destroying container")
 		if err := ctr.Destroy(); err != nil {
 			msgNPanic(ctr.Logger, errors.Join(errors.New("main: error destroying container"), err).Error())
 		}
@@ -54,26 +54,26 @@ func main() {
 
 	// start service
 	splash := strings.Join(append([]string{metadata.BaseInfo()}, metadata.BuildInfo()...), ", ")
-	ctr.Logger.Info(loggingCtx, splash)
+	ctr.Logger.Info(lctx, splash)
 
 	// start the REST server to handle requests
 	restSrv, err := rest.NewServer(cfg.Rest, ctr)
 	if err != nil {
 		msgNPanic(ctr.Logger, errors.Join(errors.New("main: error creating REST server"), err).Error())
 	}
-	ctr.Logger.Info(loggingCtx, "starting REST server")
+	ctr.Logger.Info(lctx, "starting REST server")
 	if err := restSrv.Start(); err != nil {
 		msgNPanic(ctr.Logger, errors.Join(errors.New("main: error starting REST server"), err).Error())
 	}
 	defer func() {
-		ctr.Logger.Info(loggingCtx, "stopping REST server")
+		ctr.Logger.Info(lctx, "stopping REST server")
 		if err := restSrv.Stop(); err != nil {
 			msgNPanic(ctr.Logger, errors.Join(errors.New("main: error stopping REST server"), err).Error())
 		}
 	}()
 
 	// mark service as ready to accept connections
-	ctr.Logger.Info(loggingCtx, fmt.Sprintf("ready to accept connections on port %d", cfg.Rest.Port))
+	ctr.Logger.Info(lctx, fmt.Sprintf("ready to accept connections on port %d", cfg.Rest.Port))
 
 	// enable graceful shutdown
 	sig := make(chan os.Signal, 1)
@@ -84,7 +84,7 @@ func main() {
 
 	// block until a registered signal is received
 	<-sig
-	ctr.Logger.Info(loggingCtx, "stopping service")
+	ctr.Logger.Info(lctx, "stopping service")
 	// NOTE: We have used a service shutdown pattern leveraging `defer`.
 	// This way, whenever a critical resource is created we do the destruction of that resource
 	// right after the creation with a `defer`. So when the `main` function returns or panics
